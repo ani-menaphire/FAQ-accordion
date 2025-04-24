dd6f7134972bce3fcb405023c733d4857ba705f0
Ani <131550720+ani-menaphire@users.noreply.github.com>
2025-04-24 17:56:00 +0400

    Add styles to make the design responsive

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let txtElArr = Array.from(document.querySelectorAll('.collapsed'));
+let txtElArr = Array.from(document.querySelectorAll('.content'));
 let btnElArr = Array.from(document.querySelectorAll('button'));
+let plusIconElArr = Array.from(document.querySelectorAll('.plus-icon'));
+let minusIconElArr = Array.from(document.querySelectorAll('.minus-icon'));
 
 console.log(txtElArr)
 
@@ -23,9 +25,20 @@ function hideAll() {
     for(let btn of btnElArr) {
         btn.setAttribute('aria-expanded', "false")
     }
+    
+    for(let icon of minusIconElArr) {
+        icon.classList.add('collapsed');
+    }
+
+    for(let icon of plusIconElArr) {
+        icon.classList.remove('collapsed')
+    }
 }
 
 function show(i) {
-        btnElArr[i].setAttribute('aria-expanded', "true")
+        btnElArr[i].setAttribute('aria-expanded', "true");
         txtElArr[i].classList.remove('collapsed');
+
+        plusIconElArr[i].classList.add('collapsed');
+        minusIconElArr[i].classList.remove('collapsed');
 }
